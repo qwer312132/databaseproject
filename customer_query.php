295bb66b0c77ef5436c3a4fe386d454a052298e2
roony <roony20020505@gmail.com>
2022-12-14 00:10:22 +0800

    add query function in customer_query.php

--- customer_query.php
+++ customer_query.php
@@ -54,17 +54,7 @@
         </form>
         <!-- customer_query.php -->
         <?php
-        $user = 'root';
-        $password = '123456789';
-        try {
-            $db = new PDO('mysql:host=localhost;dbname=hw3;charset=utf8', $user, $password);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        } catch (PDOException $e) {
-            print "ERROR!:" . $e->getMessage();
-            die();
-        }
-
+        include_once "db_conn.php";
 
         if (isset($_POST['button1'])) {
             $query = sprintf("select * from customer where customer_city='%s'",$_POST['city']);
