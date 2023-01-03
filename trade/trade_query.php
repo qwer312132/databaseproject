ba5825ef0dea285daad86118a90d7662d95bb348
roony <roony20020505@gmail.com>
2023-01-03 17:48:20 +0800

    fix the bug of update

--- trade/trade_query.php
+++ trade/trade_query.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Customer</title>
+    <title>Trade</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" />
